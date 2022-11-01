201e8f542d0c0de14e326322b50f7f64ba6ea7fd
Carlos <lopes.carlos@academico.ifpb.edu.br>
2022-11-01 14:31:50 -0300

    update

---
+++ Aula 09 Tarefa de Casa/equations.js
@@ -0,0 +1,19 @@
+function calcular() {
+    document.getElementById('firstequation').innerHTML = `Resultado: ${6 + 20 - 12 * 2}`;
+}
+
+function calcular2st () {
+    document.getElementById('secondequation').innerHTML = `Resultado: ${(30 / (10 + (2 + 3)))}`;
+}
+
+function calcular3st () {
+    document.getElementById('thirdequation').innerHTML = `Resultado: ${(((18 + 3 * 2) / 8 + 5 * 3) / 6)}`
+}
+
+function calcular4st () {
+    document.getElementById('fourthequation').innerHTML = `Resultado: ${(((64 - 16 * 4) + (48 * 10 - 180)) * 5)}`;
+}
+
+function calcular5st () {
+    document.getElementById('fifthequation').innerHTML = `Resultado: ${(((8 * 4 + 3) / 7 + (3 + 15 / 5) * 3) * 2 - (19 - 7) / 6) * 2 + 12}`
+}
